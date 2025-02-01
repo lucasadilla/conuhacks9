9ba568ab6f398bc83e15267edf457251a7464bad
destroyer1344 <rohitmishra1344@gmail.com>
2025-02-01 17:29:19 -0500

    moving

---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movspeed;
+    public Rigidbody2D body;
+    public Weapon weapon;
+    float speed = 5;
+    float speedX, speedY;
+    public Transform firepoint;
+    public GameObject bullet;
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        speedX = Input. GetAxisRaw("Horizontal") * speed;
+        speedY = Input. GetAxisRaw ("Vertical") * speed;
+        body.linearVelocity = new Vector2 (speedX , speedY);
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint (transform.position);
+        Vector2 mouseDistance = mousePos - screenPos;
+        float angle = Mathf.Atan2(mouseDistance.y, mouseDistance.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if (Input.GetButton("Fire1"))
+        {
+            shotCounter -= Time.deltaTime;
+            if (shotCounter > 0)
+            {
+                shotCounter = timeBetweenShots;
+                Instantiate(bullet, firepoint.position, firepoint.rotation);
+            }
+        }
+    }
+}
--- My project/Assets/Scripts/Shooting.cs
+++ My project/Assets/Scripts/Shooting.cs
@@ -1,48 +1,23 @@
 using UnityEngine;
 
-public class Shooting
+public class Bullet : MonoBehaviour
 {
-    public GameObject bulletPrefab; // Prefab of the bullet to shoot
-    public Transform firePoint;     // Point from where the bullet will be fired
-    public float bulletForce = 20f; // Speed of the bullet
-
+    public float movespeed;
+    public Rigidbody2D bullet;
+    public GameObject impactEffect;
+    void Start()
+    {
+ 
+    }
     void Update()
     {
-        // Rotate the player to face the mouse position
-        Aim();
-
-        // Check for mouse click to shoot
-        if (Input.GetButtonDown("Fire1")) // "Fire1" is the left mouse button by default
-        {
-            Shoot();
-        }
+        bullet.linearVelocity = new Vector2(1,0) * movespeed;
     }
 
-    void Aim()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Get the mouse position in world coordinates
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0; // Ensure the z-coordinate is 0 for 2D
-
-        // Calculate the direction from the player to the mouse position
-        Vector2 direction = (mousePosition - transform.position).normalized;
-
-        // Calculate the angle to rotate the player
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // Rotate the player to face the mouse position
-        transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
-
-    void Shoot()
-    {
-        // Instantiate the bullet at the fire point position and rotation
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-        // Get the Rigidbody2D component of the bullet
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-
-        // Apply force to the bullet in the direction of the fire point
-        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+        Destroy(bullet.gameObject);
+        
+        Instantiate(impactEffect, transform.position, Quaternion.identity);
     }
 }
