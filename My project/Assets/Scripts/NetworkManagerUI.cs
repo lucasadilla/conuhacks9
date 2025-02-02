3721adce1d9792336e2fcba659d7ab123e9b68b3
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-02-02 00:02:17 -0500

    started work on networkmanagerui

---
+++ My project/Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+
+}
